133ce3c1009fd341b4836828a6d3692f86db9c69
tinuola <tinuola.awopetu@gmail.com>
2018-03-20 11:54:53 -0700

    updating visual design, readme

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,7 @@
-$(document).ready(function () {
 
-  $("#sidebar").mCustomScrollbar({
-       theme: "minimal"
-  });
-
-  $('#sidebarCollapse').on('click', function () {
-      // open or close navbar
-      $('#sidebar').toggleClass('active');
-      // close dropdowns
-      $('.collapse.in').toggleClass('in');
-      // and also adjust aria-expanded attributes we use for the open/closed arrows
-      // in our CSS
-      $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-  });
-
-});
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
